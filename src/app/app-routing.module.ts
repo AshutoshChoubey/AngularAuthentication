28340b919dec2cd1ed7afe090c78e579610a2e7f
vishalAnand77_ <vishalanand.anand@gmail.com>
2020-06-23 12:24:57 +0530

    checking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,4 +11,9 @@ export const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+  constructor(){
+    console.log('here');
+  }
+}
